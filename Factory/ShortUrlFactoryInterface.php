e6cc359062bc88fd639d37b4f935889f93562b49
david <david.nurdin@respawnsive.com>
2022-06-03 10:52:14 +0200

    Missing file

---
+++ Factory/ShortUrlFactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Leopardd\Bundle\UrlShortenerBundle\Factory;
+
+use Leopardd\Bundle\UrlShortenerBundle\Entity\ShortUrlInterface;
+
+/**
+ * Interface ShortUrlFactoryInterface
+ * @package Leopardd\Bundle\UrlShortenerBundle\Factory
+ */
+interface ShortUrlFactoryInterface
+{
+    /**
+     * @see http://stackoverflow.com/questions/7003416/validating-a-url-in-php
+     * @param string $url
+     * @return ShortUrlInterface
+     */
+    public function create($url);
+}
